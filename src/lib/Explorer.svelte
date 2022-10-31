ac1668177ce39c6a04d824e08a3de754e0c3d2d4
StrixSC <talon.strix@gmail.com>
2022-10-31 00:42:18 -0400

    Updated packages (Removed useless rxdb) and added styling for the table in the explorer

--- src/lib/Explorer.svelte
+++ src/lib/Explorer.svelte
@@ -163,60 +163,52 @@
     }
 </script>
 
-<main>
+<main class="flex justify-start gap-1">
     <LeftNavbar></LeftNavbar>
     {#if $activeChain}
-        {#await qBlocks()}
+        {#await qBlocks()}<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.6424 8.35284H8.40812L11.8941 11.8388C12.0273 11.972 12.2466 11.9828 12.3835 11.8538C13.2372 11.0495 13.8244 9.96533 13.9967 8.74658C14.0263 8.53791 13.8531 8.35284 13.6424 8.35284ZM13.2932 6.92343C13.1115 4.28829 11.0058 2.18256 8.37062 2.0008C8.16944 1.9869 8.00003 2.15653 8.00003 2.35815V7.29401H12.9361C13.1377 7.29401 13.3071 7.1246 13.2932 6.92343ZM6.94121 8.35284V3.11851C6.94121 2.90785 6.75614 2.73469 6.54768 2.76425C3.91894 3.13572 1.90961 5.43226 2.00314 8.18453C2.09931 11.0111 4.53305 13.3291 7.36099 13.2936C8.47275 13.2797 9.50002 12.9214 10.3447 12.3223C10.5189 12.1988 10.5304 11.9422 10.3793 11.7911L6.94121 8.35284Z" fill="black"/>
+</svg>
+
             Loading...
         {:then blocks}
-            <div class="flex justify-center">
-                <nav>
-                    <ul class="flex list-style-none gap-2">
-                        <li>
-                            <a href="#">Previous</a>
-                        </li>
-                        <li>
-                            <a href="#">Next</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
             <div class="overflow-x-auto relative">
-                <table class="flex flex-col gap-1">
-                    <caption class="flex justify-start font-inter">Blocks</caption>
+                <div class="p-3 table-header flex justify-between">
+                    <h2 class="flex justify-start font-bold">Mined Blocks</h2>
+                    <div class="table-utilities flex justify-center align-center">
+                        <div class="search-icon-container pr-2 border-r border-gray-150 align-center justify-center flex hover:cursor-pointer">
+                            <i class="text-gray-400 flex justify-center flex-col align-center fa-solid fa-search"></i>
+                        </div>
+                        <div class="pl-2 sort-icon-container flex justify-center align-center gap-2">
+                            <i class="flex justify-center flex-col align-center fa-solid fa-chart-pie"></i>
+                            <div class="flex justify-center align-center gap-1 hover:cursor-pointer">
+                                <h2 class="font-bold"> Sort by </h2>
+                                <i class="text-gray-400 flex justify-center flex-col align-center fa-solid fa-chevron-down text-xs"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <table class="table">
                     <thead>
                         <tr>
-                            <th> Block </th>
-                            <th> Age </th>
-                            <th> Txn </th>
-                            <th> Miner </th>
-                            <th> Gas Used </th>
-                            <th> Gas Limit </th>
+                            <th class="text-center p-2 whitespace-nowrap text-xs font-thin uppercase text-headline-light-gray">Block</th>
+                            <th class="text-center p-2 whitespace-nowrap text-xs font-thin uppercase text-headline-light-gray">Age</th>
+                            <th class="text-center p-2 whitespace-nowrap text-xs font-thin uppercase text-headline-light-gray">Txn</th>
+                            <th class="text-center p-2 whitespace-nowrap text-xs font-thin uppercase text-headline-light-gray">Miner</th>
+                            <th class="text-center p-2 whitespace-nowrap text-xs font-thin uppercase text-headline-light-gray">Gas Used</th>
+                            <th class="text-center p-2 whitespace-nowrap text-xs font-thin uppercase text-headline-light-gray">Gas Limit</th>
                         </tr>
                     </thead>
                     <tbody>
                         {#each blocks as block}
-                            <tr>
-                                <th>
-                                    {block.number}
-                                </th>
-                                <td>
-                                    {moment
-                                        .unix(block.timestamp)
-                                        .format('MM/DD/YYYY HH:mm:SS:ss')}
-                                </td>
-                                <td>
-                                    {block.transactions.length}
-                                </td>
-                                <td>
-                                    {block.miner}
-                                </td>
-                                <td>
-                                    {BigNumber.from(block.gasUsed)}
-                                </td>
-                                <td>
-                                    {BigNumber.from(block.gasLimit)}
-                                </td>
+                            <tr class="border-b border-gray-150">
+                                <th class="text-center p-2 whitespace-nowrap">{block.number}</th>
+                                <td class="text-center p-2 whitespace-nowrap">{moment.unix(block.timestamp).format('MM/DD/YYYY HH:mm:SS:ss')}</td>
+                                <td class="text-center p-2 whitespace-nowrap">{block.transactions.length}</td>
+                                <td class="text-center p-2 whitespace-nowrap">{block.miner.substring(0, 8) + "..." + block.miner.substring(block.miner.length - 3, block.miner.length)}</td>
+                                <td class="text-center p-2 whitespace-nowrap">{BigNumber.from(block.gasUsed)}</td>
+                                <td class="text-center p-2 whitespace-nowrap">{BigNumber.from(block.gasLimit)}</td>
+                                <td class="text-center p-2 whitespace-nowrap underline hover:cursor-pointer">View</td>
                             </tr>
                         {/each}
                     </tbody>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,21 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     fontFamily: {
-      sans: [ "Inter", "sans-serif"],
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "headline-light-gray": '#CFD5E2',
+        "headline-gray": '#6F7277',
+        "headline-navy": '#161B25',
+        "key-blue": '#1755E7',
+        "key-white": '#F0F2F5',
+        "key-light-gray": '#C3CDD9',
+        "process-green": '#02C94F',
+        "process-orange": '#FF7E07',
+        "process-yellow": '#FFCD1C',
+        "process-red": '#F33B12',
+      },
     },
     darkMode: 'class',
     plugins: []
